78c7654e772942dce81f8ebe8af53055464d2a13
sanarguz <santiago.arcilag@gmail.com>
2021-01-22 15:42:24 -0500

    Se ha instalado React Router para manejar las rutas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListaLugares from '../components/ListaLugares'
+
+const Home = () => {
+    return (
+        <div>
+            <ListaLugares />
+        </div>
+    )
+}
+
+export default Home
